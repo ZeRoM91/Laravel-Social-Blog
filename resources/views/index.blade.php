90fbb75d9ec370b37185fa36fac2283ce56ca9ee
doroninds <dmitriy435@.gmail.com>
2017-07-07 12:04:23 +0300

    Init

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,13 +7,16 @@
     <form class="form col-md-4">
         <h1>Authorization</h1>
         <div class="form-group">
-            <label for="formGroupExampleInput">Login</label>
-            <input type="text" class="form__input form-control" placeholder="Example input">
+            <label for="login">Login</label>
+            <input type="text" class="form__input form-control" placeholder="Enter your login" name="login" required>
         </div>
         <div class="form-group">
-            <label for="formGroupExampleInput2">Password</label>
-            <input type="text" class="form__input form-control" placeholder="Another input">
+            <label for="passwd">Password</label>
+            <input type="password" class="form__input form-control" placeholder="Enter your password" name="passwd" required>
         </div>
+
+        <button class="btn btn-primary">Регистрация</button>
+        <button type="submit" class="btn btn-success">Войти</button>
     </form>
 <div class="col-md-4"></div>
 </header>

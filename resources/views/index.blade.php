f772a076f9a83dfacb34ff6fbcb48c9f8ae89030
doroninds <dmitriy435@.gmail.com>
2017-07-07 11:14:51 +0300

    Add project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('header')
+    <body>
+<header>
+
+
+<div class="col-md-4"></div>
+    <form class="form col-md-4">
+        <h1>Authorization</h1>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Login</label>
+            <input type="text" class="form__input form-control" placeholder="Example input">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput2">Password</label>
+            <input type="text" class="form__input form-control" placeholder="Another input">
+        </div>
+    </form>
+<div class="col-md-4"></div>
+</header>
+    </body>
+</html>

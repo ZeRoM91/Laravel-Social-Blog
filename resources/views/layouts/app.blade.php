c193892c392928ddf628bae5c5103efed9c0a568
doroninds <dmitriy435@gmail.com>
2017-12-19 12:25:48 +0400

    Обновления описания. Корректировка env.example

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-theme.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
 
